95fdd5380c46fbeb5d72606c86acde633a961f4d
judyfong <judyyfong@gmail.com>
2020-12-07 16:56:26 +0000

    Add aishell scripts

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ ln -sfn ../../wsj/s5/steps steps
 ln -sfn ../../wsj/s5/utils utils
 ln -sfn ../../callhome_diarization/v1/diarization diarization
 ln -sfn ../../sre08/v1/sid/ sid
+cd local
+ln -sfn $KALDI_ROOT/egs/aishell/v1/local/produce_trials.py  product_trials.py
+ln -sfn $KALDI_ROOT/egs/aishell/v1/local/split_data_enroll_eval.py  split_data_enroll_eval.py
+cd ../
 
 echo "Make logs dir"
 mkdir -p logs
